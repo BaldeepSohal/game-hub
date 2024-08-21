ec8c7a92829105248ed94bf3d143510106178693
Baldeep Kaur <baldeepsohal41@gmail.com>
2024-08-20 20:18:38 -0400

    filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -32,7 +32,11 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconsMap[platform.slug]} color="gray.500"></Icon>
+        <Icon
+          key={platform.id}
+          as={iconsMap[platform.slug]}
+          color="gray.500"
+        ></Icon>
       ))}
     </HStack>
   );
